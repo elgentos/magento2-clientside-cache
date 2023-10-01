09cb97fc425f432e18317bb7618ae6926f1d13fe
Jeroen Boersma <jeroen@elgentos.nl>
2023-10-01 11:28:52 +0200

    Added scripts and sync file

---
+++ Block/Async.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Elgentos\ClientsideCache\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Async extends Template
+{
+    protected $_template = 'Elgentos_ClientsideCache::wrapper.phtml';
+}
